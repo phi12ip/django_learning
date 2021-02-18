52ad15bc75c9d3c23bb32b166457ba7cedab029b
phi12ip <coderphilip@gmail.com>
2021-02-18 17:43:32 -0500

    autopep

--- polls/views.py
+++ polls/views.py
@@ -8,9 +8,7 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    context = {'latest_question_list': latest_question_list}
     return render(context, request)
 
 
